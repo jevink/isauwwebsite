2d9f47908bfaa63cd4f26d5a349ec285372b401e
Jevin Kosasih <jevink@uw.edu>
2021-10-17 16:43:32 -0700

    officers page working with json

--- src/components/Officers.jsx
+++ src/components/Officers.jsx
@@ -1,44 +1,14 @@
 import React from "react";
 import AboutUs from "./AboutUs";
 import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-
+import OfficersTree from "./OfficersTree";
 
 function Officers() {
     return (
         <div>
             <AboutUs />
             <Container className="text-center" style={{ gridAutoRows: "1fr" }}>
-                <Row xs={4} className="justify-content-center" style={{ display: "flex", position: "relative" }}>
-                    <Col style={{ display: "flex", background: "linear-gradient(#fddd39 0 0) bottom/100% 70% no-repeat" }}>
-                        <div style={{ position: "relative", alignSelf: "flex-end" }}>
-                            <img src="https://cdn.contrastly.com/wp-content/themes/contrastly-v9/images/anthropics-portraitpro.png" style={{ width: "70%" }}></img>
-                        </div>
-                    </Col>
-
-                    <Col style={{ display: "flex", background: "linear-gradient(#fddd39 0 0) bottom/100% 70% no-repeat" }}>
-                        <div style={{ position: "relative", alignSelf: "flex-end" }}>
-                            <img src="https://assets.grooveapps.com/images/5fff4a731d2cfa006f386042/1624073591_Professional-Headshots-Portraits-by-Jared-Wolfe-Alexandria-VA-Portrait-Studio-Testimonial-01.png" style={{ width: "70%" }}></img>
-                        </div>
-                    </Col>
-                </Row>
-
-                <Row xs={4} className="justify-content-center" style={{ display: "flex" }}>
-                    <Col style={{ padding: "0", alignSelf: "flex-end" }}>
-                        <div style={{ alignSelf: "flex-end" }}>
-                            <h3 style={{ fontWeight: "700", fontSize: `calc(12px + 0.5vw)`, marginBottom: "0" }}>President</h3>
-                            <p style={{ fontWeight: "300", fontSize: `calc(12px + 0.3vw)`, marginBottom: "0" }}>Names Goes Here</p>
-                        </div>
-                    </Col>
-
-                    <Col style={{ padding: "0", alignSelf: "flex-end" }}>
-                        <div style={{ alignSelf: "flex-end" }}>
-                            <h3 style={{ fontWeight: "700", fontSize: `calc(12px + 0.5vw)`, marginBottom: "0" }}>Vice President</h3>
-                            <p style={{ fontWeight: "300", fontSize: `calc(12px + 0.3vw)`, marginBottom: "0" }}>Names Goes Here</p>
-                        </div>
-                    </Col>
-                </Row>
+                <OfficersTree />
             </Container>
         </div>
     );
---
+++ src/components/OfficersTree.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Officers from './officers.json'
+
+function OfficersTree() {
+
+    return (
+        <div>
+            {Officers.map((row, i) => {
+                return (
+                    <div>
+                        <Row xs={3} className="justify-content-center" style={{ display: "flex" }}>
+                            {row.content.map((officer, j) => {
+                                return (
+                                    <Col style={{ display: "flex", background: "linear-gradient(#fddd39 0 0) bottom/100% 70% no-repeat" }}>
+                                        <div style={{ alignSelf: "flex-end" }}>
+                                            <img src={officer.img} style={{ width: "70%" }}></img>
+                                        </div>
+                                    </Col>
+                                )
+                            })}
+                        </Row>
+                        <Row xs={3} className="justify-content-center" style={{ display: "flex" }}>
+                            {row.content.map((officer, j) => {
+                                return (
+                                    <Col style={{ alignSelf: "flex-start" }}>
+                                        <p style={{ fontWeight: "300", fontSize: `calc(10px + 0.3vw)`, marginBottom: "0" }}>{officer.name}</p>
+                                        <h3 style={{ fontWeight: "700", fontSize: `calc(10px + 0.4vw)`, marginBottom: "0" }}>{officer.position}</h3>
+                                    </Col>
+                                )
+                            })}
+                        </Row>
+                    </div>
+                )
+            })}
+        </div>
+    );
+}
+
+export default OfficersTree;
