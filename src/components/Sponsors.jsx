744baa6ba671312ad77badba145cece001fe7a43
dgunadharma20 <dgunadharma20@gmail.com>
2021-12-29 18:55:48 -0800

    forgot to add logo images

---
+++ src/components/Sponsors.jsx
@@ -0,0 +1,55 @@
+import sponsors from '../data/sponsors.json';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+
+function Sponsors() {
+    return (
+        <div>
+            <img
+                className="d-block w-100"
+                src="https://picsum.photos/2000"
+                alt=""
+                style={{ height: "calc(50vh + 10vw)" }}
+            />
+            <h1 className="text-center m-3">University District</h1>
+            <Container className="justify-content-center">
+                <Row className="justify-content-center">
+                    {sponsors.map((sponsor) => {
+                        if (sponsor.location === "University District") {
+                            return (
+                                <Col className="text-center ml-1 mr-1" xs={5} md={3} lg={2} style={{ display: "flex" }}>
+                                    <div style={{ alignSelf: "flex-end" }}>
+                                        <img className="mx-auto" src={sponsor.img} style={{ width: `calc(100% - 12vw + 20px)` }}></img>
+                                        <h4>{sponsor.name}</h4>
+                                        <h5>{sponsor.offers}</h5>
+                                    </div>
+                                </Col>
+                            )
+                        }
+                    })}
+                </Row>
+            </Container>
+            <h1 className="text-center m-3">Other Locations</h1>
+            <Container className="justify-content-center">
+                <Row className="justify-content-center">
+                    {sponsors.map((sponsor) => {
+                        if (sponsor.location !== "University District") {
+                            return (
+                                <Col className="text-center ml-1 mr-1" xs={5} md={3} lg={2} style={{ display: "flex" }}>
+                                    <div style={{ alignSelf: "flex-end" }}>
+                                        <img className="mx-auto" src={sponsor.img} style={{ width: `calc(100% - 12vw + 20px)` }}></img>
+                                        <h4>{sponsor.name}</h4>
+                                        <h5>{sponsor.offers}</h5>
+                                    </div>
+                                </Col>
+                            )
+                        }
+                    })}
+                </Row>
+            </Container>
+        </div>
+    );
+}
+
+export default Sponsors;
