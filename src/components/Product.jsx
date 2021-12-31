3e96b26e791f8a6637bd8280a9ad3f200068a65a
ivannagwen <ivanna07@uw.edu>
2021-12-30 21:19:13 -0800

    Add products pictures

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,6 @@
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import products from '../data/products.json';
 
 function Product(props) {
     // json file to get product's name, price, size, img, etc.
@@ -7,7 +8,7 @@ function Product(props) {
     return (
         <div style={{ height: "100%" }}>
             <Card className="product-card" style={{ height: "100%" }}>
-                <Card.Img variant="top" src="https://picsum.photos/2000" />
+                <Card.Img variant="top" src={product.img} />
                 <Card.Body className="product-body" style={{ display: "flex", flexDirection: "column", height: "100%" }}>
                     <Card.Title className="product-title">{product.name}</Card.Title>
                     <Card.Subtitle className="product-desc">{product.desc}</Card.Subtitle>
