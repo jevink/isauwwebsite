d004462169785b3879a49c9375ddc319bc2653fa
Jevin Kosasih <jevink@uw.edu>
2021-12-30 22:21:04 -0800

    start change home page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,20 @@
-import AboutUs from "./AboutUs";
+import AboutUs from "./WhoWeAre";
 import Container from 'react-bootstrap/Container'
 import Officers from "./Officers";
+import VisionMission from "./VisionMission";
 
 function About() {
     return (
         <div>
-            <AboutUs />
+            <img
+                className="d-block w-100"
+                src="https://picsum.photos/2000"
+                alt=""
+                style={{ height: "calc(50vh + 10vw)" }}
+            />
+            <Container>
+                <VisionMission />
+            </Container>
             <Container className="text-center" style={{ gridAutoRows: "1fr" }}>
                 <Officers />
             </Container>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import Container from 'react-bootstrap/Container'
 import Carousel from 'react-bootstrap/Carousel';
-import VisionMission from './VisionMission';
+import WhoWeAre from './WhoWeAre';
 import InstaFeed from './InstaFeed';
 
 function Home() {
@@ -8,47 +8,21 @@ function Home() {
     return (
         <div>
             {/* Carousel */}
-            <Carousel style={{ height: "calc(50vh + 10vw)" }}>
-                <Carousel.Item interval={4000}>
-                    <img
-                        className="d-block w-100"
-                        src="https://picsum.photos/2000"
-                        alt=""
-                    />
-                    <Carousel.Caption style={{ bottom: "18%", letterSpacing: "10pt" }}>
-                        <div className="animated fadeInDown">
-                            <h1 className="carousel-title">SEATTLE 101</h1>
-                        </div>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={4000}>
-                    <img
-                        className="d-block w-100"
-                        src="https://picsum.photos/2200"
-                        alt=""
-                    />
-                    <Carousel.Caption style={{ bottom: "18%", letterSpacing: "10pt" }}>
-                        <div className="animated fadeInDown">
-                            <h1 className="carousel-title">KERATON</h1>
-                        </div>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item interval={4000}>
-                    <img
-                        className="d-block w-100"
-                        src="https://picsum.photos/2400"
-                        alt=""
-                    />
-                    <Carousel.Caption style={{ bottom: "18%", letterSpacing: "10pt" }}>
-                        <div className="animated fadeInDown">
-                            <h1 className="carousel-title">SANTAPAN</h1>
-                        </div>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+            <div>
+                <img
+                    src="https://picsum.photos/2200"
+                    alt=""
+                    style={{ height: "100vh", width: "100%", margin: "auto", objectFit: "cover" }}
+                />
+                <Carousel.Caption style={{ bottom: "35%" }}>
+                    <div className="animated fadeInDown">
+                        <h1 className="carousel-title">Indonesian Student Association at the University of Washington</h1>
+                    </div>
+                </Carousel.Caption>
+            </div>
 
             <Container>
-                <VisionMission />
+                <WhoWeAre />
             </Container>
 
             <Container>
