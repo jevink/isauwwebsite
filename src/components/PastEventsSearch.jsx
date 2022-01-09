4fe348f3a4f74256be8464369786672e348a1cc2
dgunadharma20 <dgunadharma20@gmail.com>
2022-01-08 19:13:21 -0800

    search past events, removed filtering for now

--- src/components/PastEventsSearch.jsx
+++ src/components/PastEventsSearch.jsx
@@ -4,8 +4,6 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
-import SearchFilters from './SearchFilters';
-
 // import _ from 'lodash';
 
 import events from '../data/events.json';
@@ -17,21 +15,6 @@ function PastEventsSearch() {
     const [searchText, setSearchText] = useState("");
     const [displayedData, setDisplayedData] = useState(pastEvents);
 
-    const applyFilter = (yearPicked) => {
-        if (yearPicked === '') {
-            setDisplayedData(pastEvents);
-        } else {
-            const filteredData = pastEvents.filter((event) => {
-                if (event.year === yearPicked) {
-                    return true;
-                } else {
-                    return false;
-                }
-            });
-            setDisplayedData(filteredData);
-        }
-    }
-
     const handleSearchText = (event) => {
         let value = event.target.value;
         if (value === '') {
@@ -39,40 +22,26 @@ function PastEventsSearch() {
         }
         setSearchText(value);
         const searchEvent = pastEvents.filter((event) => {
-            return event.title.toLowerCase().includes(value.toLowerCase());
+            return (event.title.toLowerCase().includes(value.toLowerCase()) ||
+                event.text.toLowerCase().includes(value.toLowerCase()));
         });
         setDisplayedData(searchEvent);
 
     };
-    const uniqueYearOptions = [...new Set(pastEvents.reduce((all, current) => {
-        return all.concat([current.year]);
-    }, []))].sort().reverse();
-
     return (
         <div>
             <Container className="justify-content-center">
                 <Row className="justify-content-between">
                     <Col className="col-8">
                         <form className="ml-2">
-                            <textarea
-                                className="searchText form-control"
-                                onChange={handleSearchText}
-                                value={searchText}
-                                placeholder="Search a Past Event"
-                                rows="1"
-                                style={{ resize: "none" }}
-                                onKeyPress={e => {
-                                    if (e.key === 'Enter')
-                                        e.preventDefault()
-                                }}
-                            />
+                            <div className="form-group mb-2">
+                                <label htmlFor="searchQuery" className="mb-2">What events do you want to see?</label>
+                                <input type="text" className="searchText form-control" placeholder="Search For a Past Event"
+                                    value={searchText} onChange={handleSearchText} />
+                            </div>
                         </form>
                     </Col>
                     <Col className="col-2">
-                        <SearchFilters 
-                            yearOptions={uniqueYearOptions} 
-                            filterCallback={applyFilter}
-                        />
                     </Col>
                 </Row>
             </Container>
