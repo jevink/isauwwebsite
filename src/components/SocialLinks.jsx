58da24686512267000fe85970466c4796ef01eba
Jevin Kosasih <jevink@uw.edu>
2022-03-05 22:29:21 -0800

    fix social icon styling

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -4,10 +4,10 @@ import { FaInstagram, FaYoutube, FaFacebookSquare, FaTiktok } from 'react-icons/
 function SocialLinks() {
     return (
         <div className="nav-social-media">
-            <a href="https://www.instagram.com/isauwhuskies/?hl=en" className="social" target="_blank"> <FaInstagram className="navIcon" /> </a>
-            <a href="https://www.youtube.com/user/isauwHuskies" className="social" target="_blank"> <FaYoutube className="navIcon" /> </a>
-            <a href="https://www.facebook.com/isauw.huskies/" className="social" target="_blank"> <FaFacebookSquare className="navIcon" /> </a>
-            <a href="https://www.tiktok.com/@isauwhuskies" className="social" target="_blank"> <FaTiktok className="navIcon" /> </a>
+            <a href="https://www.instagram.com/isauwhuskies/?hl=en" className="social" target="_blank"> <FaInstagram className="nav-social-icon" /> </a>
+            <a href="https://www.youtube.com/user/isauwHuskies" className="social" target="_blank"> <FaYoutube className="nav-social-icon" /> </a>
+            <a href="https://www.facebook.com/isauw.huskies/" className="social" target="_blank"> <FaFacebookSquare className="nav-social-icon" /> </a>
+            <a href="https://www.tiktok.com/@isauwhuskies" className="social" target="_blank"> <FaTiktok className="nav-social-icon" /> </a>
         </div>
     )
 }
