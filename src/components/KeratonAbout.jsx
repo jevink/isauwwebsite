14436a9486f586ff199675e2cf6ed443ae28ddc6
ivannagwen <ivanna07@uw.edu>
2022-04-23 13:29:57 -0700

    Add Lullaboy pictures and update about section

--- src/components/KeratonAbout.jsx
+++ src/components/KeratonAbout.jsx
@@ -9,10 +9,12 @@ function KeratonAbout() {
             <Container className="keraton-about">
                 <h1 className="mt-5 keraton-about-header"><strong>ABOUT</strong></h1>
                 <p className="keraton-about-text">
-                    Keraton is an annual Indonesian cultural festival hosted by ISAUW (Indonesian
-                    Student Association at University of Washington) with a purpose to introduce the
-                    rich culture of Indonesia to the greater Seattle Community. This event is a free
-                    one day pass to Indonesia filled with food, games, music and performances.
+                Keraton is ISAUW’s largest and most iconic event that is held annually every Spring since 2002 at the University of Washington.
+                This year will be a huge comeback of the event after the two-year pause due to COVID-19. However, we are hoping to come back stronger than ever
+                as we commit to leave an undeniable impression to a crowd of over 3,000 international audiences. We aim to provide in-depth and hands-on experience
+                of Indonesian culture through traditional performances and activities. Keraton has grown much since its start in 2002, from a small crowd of
+                70 attendees to a staggering audience of over 3,000 visitors in its latest display, consisting of people from various backgrounds.
+                This event is currently the largest of its kind in the PNW, and the second largest in the United States.
                 </p>
             </Container>
         </div>
