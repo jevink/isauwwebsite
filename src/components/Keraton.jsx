2d74acb822209b2c15a2857fae857f865111c866
Jevin Kosasih <jevink@uw.edu>
2022-04-23 22:25:46 -0700

    fix merge

--- src/components/Keraton.jsx
+++ src/components/Keraton.jsx
@@ -8,12 +8,8 @@ import KeratonVideo from './KeratonVideo';
 import KeratonAbout from './KeratonAbout';
 import KeratonPerformers from './KeratonPerformers';
 import KeratonActivities from './KeratonActivities';
-<<<<<<< HEAD
-import KeratonGetInvolved from './KeratonGetInvolved'
-import KeratonVendors from './KeratonVendors';
-=======
 import KeratonFAQ from './KeratonFAQ';
->>>>>>> 052ebad06c160fcd097617bbc193832e6ad839d6
+import KeratonVendors from './KeratonVendors';
 
 
 function Keraton() {
@@ -22,12 +18,9 @@ function Keraton() {
         <div>
             <KeratonVideo></KeratonVideo>
             <KeratonAbout></KeratonAbout>
-<<<<<<< HEAD
-            <KeratonVendors></KeratonVendors>
-=======
             <KeratonPerformers></KeratonPerformers>
+            <KeratonVendors></KeratonVendors>
             <KeratonFAQ></KeratonFAQ>
->>>>>>> 052ebad06c160fcd097617bbc193832e6ad839d6
         </div>
     );
 }
--- src/components/KeratonVendors.jsx
+++ src/components/KeratonVendors.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useRef } from 'react';
 import Slider from "react-slick";
 
@@ -78,7 +77,7 @@ function KeratonVendors() {
                             })}
                         </Slider>
                     </Col>
-                    <Col className="col-9">
+                    <Col className="col-8">
                         <Slider className="vendor-image-slider" ref={(slider2) => setNav2(slider2)} {...bigSettings}>
                             {vendorList.map((category) => {
                                 return (
@@ -115,94 +114,8 @@ function KeratonVendors() {
                     </Col>
                 </Row>
             </Container>
-=======
-import React, { useState, useEffect, useRef } from 'react';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
-
-import vendors from '../data/keraton-vendors.json';
-
-function KeratonVendors() {
-    const [numOfItems, setNumOfItems] = useState(3);
-    React.useEffect(() => {
-        window.addEventListener('resize', () => {
-            if(window.innerWidth < 900) {
-                setNumOfItems(1);
-            } else {
-                setNumOfItems(3);
-            }
-        })
-    })
-    return (
-        <div>
-            <Container className="keraton-food">
-                <h1 className="my-3 keraton-food-header"><strong>VENDORS</strong></h1>
-                <Col xs={2}>
-                    <div className="vendors-name">
-                        {vendors.map((vendor) => {
-                            return <VendorName vendor={vendor}></VendorName>
-                        })}
-                    </div>
-                </Col>
-                <OwlCarousel className='owl-theme' margin={10} items={numOfItems} mouseDrag={true} pullDrag={true}>
-                    {vendors.map((vendor) => {
-                        return <VendorCard vendor={vendor}></VendorCard>
-                    })}
-                </OwlCarousel>
-            </Container>
         </div>
     )
 }
 
-function VendorCard(props) {
-    const [isFlipped, setIsFlipped] = useState(false);
-
-    const { vendor } = props;
-
-    const front = useRef();
-    const back = useRef();
-
-    const menuItems = vendor.menu.map((item) => {
-        return <Card.Subtitle className="keraton-food-sub-title my-1">{"• " + item.name.toUpperCase() + " •"}</Card.Subtitle>;
-    });
-
-    return (
-        <div className="item">
-            <Card className={`keraton-food-card ${isFlipped ? 'flip' : ''}`} onClick={() => { setIsFlipped(!isFlipped) }} style={{ height: "420px" }}>
-                <div className="front" ref={front}>
-                    <Card.Title className="keraton-food-title">{vendor.name.toUpperCase()}</Card.Title>
-                </div>
-                <div className="back" ref={back}>
-                    <div className="back-card-header my-1">
-                        <img src={vendor.img} alt="" className="my-2" />
-                    </div>
-                    <div class="back-card-body my-3">
-                        {menuItems}
-                    </div>
-                </div>
-            </Card>
-        </div>
-    )
-}
-
-function VendorName(props) {
-    const { vendor } = props;
-    return (
-        <div className="vendors-name">
-            <a>{vendor.name.toUpperCase()}</a>
->>>>>>> 052ebad06c160fcd097617bbc193832e6ad839d6
-        </div>
-    )
-}
-
-<<<<<<< HEAD
 export default KeratonVendors;
-=======
-export default KeratonVendors
->>>>>>> 052ebad06c160fcd097617bbc193832e6ad839d6
