12e53e778d7d2c292ebd2520e4732f74d93e93a9
Jevin Kosasih <jevink@uw.edu>
2022-04-24 01:45:44 -0700

    add faq styling

--- src/components/KeratonFAQ.jsx
+++ src/components/KeratonFAQ.jsx
@@ -11,9 +11,9 @@ function KeratonFAQ() {
     }) 
     return (
         <div className="keraton-faq">
-            <h1 className="pt-5 keraton-faq-header">Frequently Asked Questions</h1>
             <Container>
-                <Accordion defaultActiveKey="0" flush className="py-5 keraton-faq-accordion">
+                <h1 className="pt-5 keraton-faq-header keraton-section-header">Frequently Asked Questions</h1>
+                <Accordion defaultActiveKey="0" flush className="keraton-faq-accordion">
                     {questionItems}
                 </Accordion>
             </Container>
