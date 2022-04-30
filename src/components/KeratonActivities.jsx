2c16a8cd587c81b0fb0c72661473771a611fa2fd
Jevin Kosasih <jevink@uw.edu>
2022-04-30 15:52:12 -0700

    activities mostly done

--- src/components/KeratonActivities.jsx
+++ src/components/KeratonActivities.jsx
@@ -8,37 +8,28 @@ function KeratonActivities() {
     return(
         <div className="keraton-section-header">
             <h1 className="my-1 keraton-section-header"><strong>Games</strong></h1>
-            <Row>
-                <Col xs={4} style={{padding: "0", margin: "0"}}>
+            <Row className="target" style={{padding: "0", margin: "0", overflow: "hidden"}}>
+                <Col xs={4} style={{padding: "0", margin: "0", overflow: "hidden"}}>
                     <div className='activity-container'>
-                        <div class="keraton-activities-overlay2">
-                        <img src="../images/bakiak.jpeg" alt="games_image" className="keraton-activities-image"></img>
-                            <p className="keraton-activities-text2">1</p>
-                        </div>
-                        <div class="keraton-activities-overlay">
-                            <h2 className="keraton-activities-text">1</h2>
+                        <img src="../images/bakiak.jpeg" className="keraton-activities-image"></img>
+                        <div style={{position: "relative"}}>
+                            <p className="keraton-activities-text">1</p>
                         </div>
                     </div>
                 </Col>
-                <Col xs={4} style={{padding: "0", margin: "0"}}>
+                <Col xs={4} style={{padding: "0", margin: "0", overflow: "hidden"}}>
                     <div className='activity-container'>
-                        <div class="keraton-activities-overlay2">
-                            <img src="../images/sack.jpeg" alt="games_image" className="keraton-activities-image"></img>
-                            <p className="keraton-activities-text2">2</p>
-                        </div>
-                        <div class="keraton-activities-overlay">
-                            <h2 className="keraton-activities-text">2</h2>
+                        <img src="../images/sack.jpeg" className="keraton-activities-image"></img>
+                        <div style={{position: "relative"}}>
+                            <p className="keraton-activities-text">2</p>
                         </div>
                     </div>
                 </Col>
-                <Col xs={4} style={{padding: "0", margin: "0"}}>
+                <Col xs={4} style={{padding: "0", margin: "0", overflow: "hidden"}}>
                     <div className='activity-container'>
-                        <div class="keraton-activities-overlay2">
-                        <img src="../images/hopscotch.jpeg" alt="games_image" className="keraton-activities-image"></img>
-                            <p className="keraton-activities-text2">3</p>
-                        </div>
-                        <div class="keraton-activities-overlay">
-                            <h2 className="keraton-activities-text">3</h2>
+                        <img src="../images/hopscotch.jpeg" className="keraton-activities-image"></img>
+                        <div style={{position: "relative"}}>
+                            <p className="keraton-activities-text">3</p>
                         </div>
                     </div>
                 </Col>
