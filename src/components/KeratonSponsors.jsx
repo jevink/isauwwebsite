e24521576a68cd08c6d9a0cf10411190dc917215
ivannagwen <ivanna07@uw.edu>
2022-05-03 16:17:20 -0700

    fix sponsors sizing

--- src/components/KeratonSponsors.jsx
+++ src/components/KeratonSponsors.jsx
@@ -9,20 +9,20 @@ function KeratonFAQ() {
             <Container >
                 <Row className="text-center keraton-sponsors-row">
                     <Col>
-                        <img src="../images/keraton/sponsors/1-csf.png" style={{maxWidth: "40%"}}></img>
+                        <img src="../images/keraton/sponsors/1-csf.png" style={{maxWidth: "20%"}}></img>
                     </Col>
                 </Row>
                 <Row className="text-center keraton-sponsors-row">
                     <Col>
-                        <img src="../images/keraton/sponsors/2-accolade.png" style={{maxWidth: "35%", margin: "0px 30px"}}></img>
-                        <img src="../images/keraton/sponsors/2-deltagro.png" style={{maxWidth: "35%", margin: "0px 30px"}}></img>
+                        <img src="../images/keraton/sponsors/2-accolade.png" style={{maxWidth: "18%", margin: "0px 30px"}}></img>
+                        <img src="../images/keraton/sponsors/2-deltagro.png" style={{maxWidth: "18%", margin: "0px 30px"}}></img>
                     </Col>
                 </Row>
                 <Row className="text-center keraton-sponsors-row">
                     <Col>
-                        <img src="../images/keraton/sponsors/3-asuw.png" style={{maxWidth: "30%"}}></img>
-                        <img src="../images/keraton/sponsors/3-asmaja.png" style={{maxWidth: "30%"}}></img>
-                        <img src="../images/keraton/sponsors/3-tgi.png" style={{maxWidth: "30%"}}></img>
+                        <img src="../images/keraton/sponsors/3-asuw.png" style={{maxWidth: "18%"}}></img>
+                        <img src="../images/keraton/sponsors/3-asmaja.png" style={{maxWidth: "14%"}}></img>
+                        <img src="../images/keraton/sponsors/3-tgi.png" style={{maxWidth: "18%"}}></img>
                     </Col>
                 </Row>
                 <Row className="text-center keraton-sponsors-row">
@@ -32,9 +32,9 @@ function KeratonFAQ() {
                         <img src="../images/keraton/sponsors/4-kjri.png" style={{width: `10%`, margin: "5px 20px"}}></img>
                     </Col>
                     <Col sm={6} style={{margin: "0", padding: "0"}} className="sponsor-right">
-                        <img src="../images/keraton/sponsors/4-uwaa.png" style={{width: `8%`, margin: "5px 20px"}}></img>
-                        <img src="../images/keraton/sponsors/4-indomie.png" style={{maxWidth: "11%", margin: "5px 20px"}}></img>
-                        <img src="../images/keraton/sponsors/4-uwsao.png" style={{width: `11%`, margin: "5px 20px"}}></img>
+                        <img src="../images/keraton/sponsors/4-uwaa.png" style={{width: `10%`, margin: "5px 20px"}}></img>
+                        <img src="../images/keraton/sponsors/4-indomie.png" style={{maxWidth: "10%", margin: "5px 20px"}}></img>
+                        <img src="../images/keraton/sponsors/4-uwsao.png" style={{width: `10%`, margin: "5px 20px"}}></img>
                     </Col>
                 </Row>
             </Container>
