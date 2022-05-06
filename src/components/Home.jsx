e2ec4e76cc09d881d673fca6082a48f834bc0055
Jevin Kosasih <jevink@uw.edu>
2022-05-06 09:32:08 -0700

    fix unsecure issue

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
             <div>
                 <div className="navbar-overlay">
                     <video playsinline="playsinline" autoplay="autoplay" muted loop style={{ height: "100vh", width: "100%", margin: "auto", objectFit: "cover" }}>
-                        <source src="http://students.washington.edu/isauw/images/cabin.mp4" type="video/mp4"></source>    
+                        <source src="https://students.washington.edu/isauw/images/cabin.mp4" type="video/mp4"></source>    
                     </video>
                 </div>
 
