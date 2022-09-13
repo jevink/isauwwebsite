18f1620f080fd20e476b3d3551099705a8bc51c0
Jevin Kosasih <jevink@uw.edu>
2022-09-13 11:10:21 -0700

    fix!: made overlay cover entire video on mobile phones with bottom navbars | might cause issues when webkit isn't available

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,8 +15,8 @@ function Home() {
         <div>
             {/* Carousel */}
             <div>
-                <div className="navbar-overlay">
-                    <video playsinline="playsinline" autoplay="autoplay" muted loop style={{ height: "100vh", width: "100%", margin: "auto", objectFit: "cover", display: "block" }}>
+                <div className="navbar-overlay" style={{height: "100vh"}}>
+                    <video playsinline="playsinline" autoplay="autoplay" muted loop style={{ minHeight: "-webkit-fill-available", width: "100%", margin: "auto", objectFit: "cover", display: "block" }}>
                         <source src="https://students.washington.edu/isauw/images/cabin.mp4" type="video/mp4"></source>    
                     </video>
                 </div>
