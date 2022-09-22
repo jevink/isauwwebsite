bbbef13ee47325603d706e84b1f57e890f215cec
Jevin Kosasih <jevink@uw.edu>
2022-09-22 01:15:38 -0700

    fix: added validation icons to upload button

--- src/components/Apply.jsx
+++ src/components/Apply.jsx
@@ -53,8 +53,13 @@ function Apply() {
     const fileObj = e.target.files && e.target.files[0];
     if (fileObj) {
       setResumeFile(fileObj);
-      if (validated) {
+      if (validated) { // validated and file is uploaded
+        // set upload button color to green, and add check mark
         const color = window.getComputedStyle(document.getElementById("resume")).borderColor;
+        document.getElementById("fakeResume").style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23198754' d='M2.3 6.73L.6 4.53c-.4-1.04.46-1.4 1.1-.8l1.1 1.4 3.4-3.8c.6-.63 1.6-.27 1.2.7l-4 4.6c-.43.5-.8.4-1.1.1z'/%3e%3c/svg%3e")`;
+        document.getElementById("fakeResume").style.backgroundPosition = `right 0.75rem center,center right 2.25rem`;
+        document.getElementById("fakeResume").style.backgroundSize = `calc(0.75em + 0.375rem) calc(0.75em + 0.375rem)`;
+        document.getElementById("fakeResume").style.backgroundRepeat = `no-repeat`;
         document.getElementById("fakeResume").style.borderColor = color;
         document.getElementById("fakeResumeText").style.color = color;
       }
@@ -142,9 +147,18 @@ function Apply() {
     }
   }
 
+  // show styling validation on the upload resume button
   useEffect(() => {
     if (validated) {
       const color = window.getComputedStyle(document.getElementById("resume")).borderColor;
+      if (color === 'rgb(25, 135, 84)') { // if color is green, show check mark
+        document.getElementById("fakeResume").style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23198754' d='M2.3 6.73L.6 4.53c-.4-1.04.46-1.4 1.1-.8l1.1 1.4 3.4-3.8c.6-.63 1.6-.27 1.2.7l-4 4.6c-.43.5-.8.4-1.1.1z'/%3e%3c/svg%3e")`;
+      } else { // else show warning icon
+        document.getElementById("fakeResume").style.backgroundImage = `url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 12 12' width='12' height='12' fill='none' stroke='%23dc3545'%3e%3ccircle cx='6' cy='6' r='4.5'/%3e%3cpath stroke-linejoin='round' d='M5.8 3.6h.4L6 6.5z'/%3e%3ccircle cx='6' cy='8.2' r='.6' fill='%23dc3545' stroke='none'/%3e%3c/svg%3e")`;
+      }
+      document.getElementById("fakeResume").style.backgroundSize = `calc(.75em + .375rem) calc(.75em + .375rem)`;
+      document.getElementById("fakeResume").style.backgroundPosition = `right 0.75rem center,center right 2.25rem`;
+      document.getElementById("fakeResume").style.backgroundRepeat = `no-repeat`;
       document.getElementById("fakeResume").style.borderColor = color;
       document.getElementById("fakeResumeText").style.color = color;
     }
